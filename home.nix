76c8514b5ec17c1cd7eb4574b162e526f16f4b95
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2026-06-21 19:26:11 +0700

    add obs studio

--- home.nix
+++ home.nix
@@ -237,5 +237,14 @@ in
     };
   };
 
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
+
   home.stateVersion = "26.05";
 }
